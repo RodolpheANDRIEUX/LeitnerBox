7ef7ddc05adfa2514ef104b5e019cc434bedcb0a
PIET Thomas <thomas.piet@ynov.com>
2023-09-27 13:34:05 +0200

    create databse and model
    aiven host

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,75 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<main>
+    <h1>Formulaire de validation</h1>
+
+    <div>
+        <label for="inputQuestion">Question :</label>
+        <input type="text"  id="inputQuestion" name="inputQuestion">
+    </div>
+
+    <div>
+        <label for="inputAnswer">Réponse :</label>
+        <input type="text"  id="inputAnswer" name="inputAnswer">
+    </div>
+
+    <button id="submitButton">Valider</button>
+</main>
+
+
+<style>
+    main {
+        text-align: center;
+        margin: 20px;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 8px;
+    }
+
+    input {
+        width: 100%;
+        padding: 8px;
+        margin-bottom: 16px;
+    }
+
+    button {
+        background-color: #007bff;
+        color: #fff;
+        padding: 10px 20px;
+        border: none;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: #0056b3;
+    }
+</style>
+
+
+<!--<script>-->
+<!--    let inputQuestion ='';-->
+<!--    let inputAnswer = '';-->
+<!--    const submitButton = document.getElementById('submitButton');-->
+
+
+<!--    submitButton.addEventListener('click', sendData);-->
+<!--    async function sendData() {-->
+<!--        console.log("test");-->
+
+<!--        const response = await fetch('/api/save-data', {-->
+<!--            method: 'POST',-->
+<!--            headers: {-->
+<!--                'Content-Type': 'application/json',-->
+<!--            },-->
+<!--            body: JSON.stringify({question: inputQuestion, answer: inputAnswer}),-->
+<!--        });-->
+<!--        if (response.ok) {-->
+<!--            alert("Data send with success"); //remplacer par un console.log-->
+<!--        } else {-->
+<!--            alert("Error HTTP: " + response.status); //remplacer par un console.log-->
+<!--        }-->
+
+<!--    }-->
+<!--</script>-->
