6d6eb96150d18c408202d96f75709f9aa8389396
PIET Thomas <thomas.piet@ynov.com>
2023-10-02 13:53:03 +0200

    feat: save

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,15 @@
 // @todo faire une fonction load pour afficher les cards
 
-import {db} from '$lib/db';
 import {Prisma} from "@prisma/client";
+import {db} from '$lib/db';
+
+export const load = async () => {
+    const cards = await db.cards.findMany(
+        {where: {id: 1}}
+    );
+    console.log("cards", cards)
+    return {cards}
+}
 
 export const actions = {
     default: async (request) => {
@@ -18,16 +26,16 @@ export const actions = {
             })
             console.log("Card created", createdCard) //debug
 
-        }  catch (e) {
+        } catch (e) {
             if (e instanceof Prisma.PrismaClientKnownRequestError) {
                 return {
                     status: 400,
-                    body: { error: true, message: e.message }
+                    body: {error: true, message: e.message}
                 };
             }
             return {
                 status: 500,
-                body: { error: true, message: 'An unknown error occurred' }
+                body: {error: true, message: 'An unknown error occurred'}
             };
         }
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,27 @@
 <!--@todo : notification pour confirmer la création de la carte-->
 <!--@todo : utiliser des prefetch onmouseover https://kit.svelte.dev/docs/link-options  https://www.youtube.com/watch?v=EQy-AYhZIlE&ab_channel=Huntabyte-->
-
 <script>
-
+export let cards = [];
+console.log(cards.answer);
 </script>
+
 
 
 <main>
     <h1>Formulaire de validation</h1>
+
+    {#if cards}
+        {#each cards as { question, answer }}
+            <div>
+                <h2>Carte</h2>
+                <p>Question : {question}</p>
+                <p>Réponse : {answer}</p>
+            </div>
+        {/each}
+    {:else}
+        <p>Loading cards...</p>
+    {/if}
+
 
     <form method="post">
 
