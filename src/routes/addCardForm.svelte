51a967828b3190433e9a78f490e8f209155efa7b
Rodolphe <rodolphe.andrieux@ynov.com>
2023-11-04 16:35:04 +0100

    added textarea as answer input and a type file for the futur

--- src/routes/addCardForm.svelte
+++ src/routes/addCardForm.svelte
@@ -21,7 +21,7 @@
 </script>
 
 <div id="container" transition:fade={{ duration: 200 }} >
-    <div id="title" transition:slide={{ duration: 200 }}>Add a new card</div>
+    <div id="title" transition:slide={{ duration: 200 }}>Add a <span>new card</span></div>
 
     {#if errorMessage}
         <p>{errorMessage}</p>
@@ -30,15 +30,20 @@
     <form on:submit|preventDefault={handleSubmit}>
         <div class="input-group">
             <label for="question">Question</label>
-            <input class={$lightMode ? "light-mode" : ""} id="question" autocomplete="off" />
+            <input class={$lightMode ? "light-mode" : ""} id="question" autocomplete="off" required />
         </div>
 
         <div class="input-group">
             <label for="answer">Answer</label>
-            <input class={$lightMode ? "light-mode" : ""} id="answer" autocomplete="off" />
+            <textarea class={$lightMode ? "light-mode" : ""} id="answer" autocomplete="off" rows="5" required></textarea>
         </div>
 
-        <button class={$lightMode ? "light-mode" : ""} type="submit" transition:slide={{ duration: 200 }}>Submit</button>
+        <div class="input-group">
+            <label for="file">Add image?</label>
+            <input type="file" id="file" name="file" required>
+        </div>
+
+        <button class={$lightMode ? "light-mode" : ""} type="submit" transition:slide={{ duration: 200 }}>Add</button>
     </form>
 </div>
 
@@ -48,6 +53,11 @@
     #title {
         font-size: 1.2rem;
         letter-spacing: .2rem;
+        margin-bottom: 2rem;
+    }
+
+    #title span {
+        color: #ff0000;
     }
 
     #container{
@@ -67,6 +77,7 @@
 
     .input-group{
         margin: 20px;
+        display: block;
     }
 
     button {
@@ -89,16 +100,28 @@
         border-color: #1a73e8;
     }
 
-    input {
-        width: 80%;
+    input, textarea {
+        display: block;
+        width: calc(100% - 2rem);
         color: white;
         border: solid 2px #9e9e9e;
         border-radius: 1rem;
         background: none;
         padding: 1rem;
         font-size: 1rem;
-        letter-spacing: .1rem;
-        transition: border 150ms cubic-bezier(0.4,0,0.2,1);
+        letter-spacing: .2rem;
+        resize: none;
+    }
+
+    textarea::-webkit-scrollbar {
+        display: none;
+    }
+
+    textarea {
+        font-family: 'Arial', sans-serif;
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+        overflow-y: scroll;
     }
 
      input.light-mode  {
@@ -106,10 +129,12 @@
     }
 
     label {
+        display: block;
+        margin-bottom: .5rem;
         color: #9e9e9e;
         letter-spacing: .1rem;
-        transform: translateY(1rem);
-        transition: 150ms cubic-bezier(0.4,0,0.2,1);
+        text-align: left;
+        padding-left: 1rem;
     }
 
 </style>
